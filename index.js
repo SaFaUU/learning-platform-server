cce76c34326a603791787e2cb20b95a26384d6b7
SaFaUU <thulk283@gmail.com>
2022-10-27 00:56:11 +0600

    Created Courses and Checkout Page

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
+app.get('/courses/:courseId', (req, res) => {
+    const id = parseInt(req.params.courseId);
+    res.send(courses[id])
+})
+app.get('/checkout/:id', (req, res) => {
+    const id = req.params.id;
+    const selected_course = courses.find(course => course.id == id);
+    res.send(selected_course)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
